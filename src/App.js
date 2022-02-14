17477417ef9ebabcd0ebcbbf94ade230c44d4cf3
Adam El Amrani <adelamco1091@outlook.com>
2022-02-14 20:25:04 +0100

    Created tasklist component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import TaskList from "./components/TaskList/TaskList";
+
 function App() {
-  return;
+  return (
+    <>
+      <main>
+        <h1>To-Dos list</h1>
+        <ul>
+          <TaskList></TaskList>
+        </ul>
+      </main>
+    </>
+  );
 }
 
 export default App;
