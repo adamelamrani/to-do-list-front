2d4289c5170cbeb079d12d85ceef7454bd41a8ed
Adam El Amrani <adelamco1091@outlook.com>
2022-02-15 00:43:19 +0100

    Renamed loadTasksListThunk file

--- src/redux/thunk/loadTasksListThunk.js
+++ src/redux/thunk/loadTasksListThunk.js
@@ -1,10 +1,10 @@
 import { loadTasksAction } from "../actions/actionCreator/actionCreator";
 
-const loadTasksList = async (dispatch) => {
+const loadTasksListThunk = async (dispatch) => {
   const response = await fetch(process.env.REACT_APP_API_URL);
   const tasksList = await response.json();
 
   dispatch(loadTasksAction(tasksList));
 };
 
-export default loadTasksList;
+export default loadTasksListThunk;
