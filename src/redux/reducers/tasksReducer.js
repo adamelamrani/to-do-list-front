77303ed1e704f579740874f9e66b95a8c7410a2e
Adam El Amrani <adelamco1091@outlook.com>
2022-02-14 23:27:20 +0100

    Added addtask and default to reducer switch cases

--- src/redux/reducers/tasksReducer.js
+++ src/redux/reducers/tasksReducer.js
@@ -1,3 +1,18 @@
+import actionTypes from "../actions/actionTypes/actionTypes";
+
 const tasksReducer = (currentTasks = [], action = {}) => {
-  switch(actionTy )
-}
+  let newTasks;
+  switch (action.type) {
+    case actionTypes.loadTasks:
+      newTasks = [...action.loadTasks];
+      break;
+    case actionTypes.addTasks:
+      newTasks = currentTasks.filter((task) => action.id !== task.id);
+      break;
+    default:
+      newTasks = [...currentTasks];
+  }
+  return newTasks;
+};
+
+export default tasksReducer;
