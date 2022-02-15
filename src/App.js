c7e861a810e344a4070c8a3a73e569490d232c30
Adam El Amrani <adelamco1091@outlook.com>
2022-02-16 00:14:39 +0100

    Added basic style

--- src/App.js
+++ src/App.js
@@ -7,8 +7,17 @@ import {
   deleteTaskThunk,
   loadTasksListThunk,
 } from "./redux/thunk/loadTasksListThunk";
+import styled from "styled-components";
 
 function App() {
+  const StyledMain = styled.main`
+    margin: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 5px 5px 5px 0px #000000, inset 4px 4px 15px 0px #000000,
+      5px 5px 15px 5px rgba(0, 0, 0, 0);
+  `;
   const todoList = useSelector((state) => state.tasksList);
   const dispatch = useDispatch();
   const deleteDispatch = (id) => {
@@ -21,7 +30,7 @@ function App() {
 
   return (
     <>
-      <main>
+      <StyledMain>
         <h1>To-Dos list</h1>
         <InputTask />
         <ul>
@@ -30,7 +39,7 @@ function App() {
             onClickAction={() => deleteDispatch(todoList[0].id)}
           />
         </ul>
-      </main>
+      </StyledMain>
     </>
   );
 }
