284411e23f54db9435421b76c2e54668cd0340f4
Adam El Amrani <adelamco1091@outlook.com>
2022-02-15 20:04:46 +0100

    Created mocks to test thunks

---
+++ src/mocks/handlers.js
@@ -0,0 +1,14 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get(process.env.REACT_APP_API_URL, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        id: 1,
+        task: "mock a function",
+        done: true,
+      })
+    );
+  }),
+];
